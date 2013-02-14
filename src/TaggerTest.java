a8a5b4e8a2a6771e3a43cb10a4ae0412bf9df4fe
Pēteris Paikens <PeterisP@gmail.com>
2013-02-14 17:03:35 +0200

    test

---
+++ src/TaggerTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TaggerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
