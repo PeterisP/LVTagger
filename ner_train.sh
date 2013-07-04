29c5ec3ff666e13ecf114f16bd25953ab736919c
Pēteris Paikens <PeterisP@gmail.com>
2013-07-04 12:36:35 +0300

    test results

--- ner_train.sh
+++ ner_train.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 java -mx2g -Dfile.encoding=utf-8 -cp "dist/CRF.jar" edu.stanford.nlp.ie.crf.CRFClassifier -prop lv-ner-train.prop
+
+java -mx1g -Dfile.encoding=utf-8 -cp "dist/CRF.jar" edu.stanford.nlp.ie.crf.CRFClassifier -prop lv-ner-tag.prop > "ner_train_comparison.txt"
