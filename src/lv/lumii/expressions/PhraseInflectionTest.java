06d95fdfbbeef0fdaaa810d52551ea9f33a4a940
Pēteris Paikens <PeterisP@gmail.com>
2014-10-23 15:32:42 +0300

    Updated copyright notices

--- src/lv/lumii/expressions/PhraseInflectionTest.java
+++ src/lv/lumii/expressions/PhraseInflectionTest.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2013,2014 Institute of Mathematics and Computer Science, University of Latvia
+ * Author: Pēteris Paikens
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package lv.lumii.expressions;
 
 import static org.junit.Assert.*;
@@ -432,5 +449,20 @@ public class PhraseInflectionTest
 	public void x20140703() throws Exception {
 		assertEquals("Andris Bērziņš", 
 				new Expression("Andra Bērziņa", "person", false).normalize());
+	}
+
+	@Test
+	public void LETA_personas_20140819() throws Exception {
+		assertEquals("Kristiānai Lībanei", 
+				new Expression("Kristiāna Lībane", "person", true).inflect(AttributeNames.v_Dative, true));
+	}
+	
+	@Test 
+	public void LETA_personas_20141023() throws Exception {
+		assertEquals("Jurijam Sovam", 
+				new Expression("Jurijs Sova", "person", true).inflect(AttributeNames.v_Dative, true));
+		
+		assertEquals("Gunaram Šķēlem", 
+				new Expression("Gunars Šķēle", "person", true).inflect(AttributeNames.v_Dative, true));
 	}
 }
--- src/lv/lumii/ner/NerPipe.java
+++ src/lv/lumii/ner/NerPipe.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright 2013,2014 Institute of Mathematics and Computer Science, University of Latvia
+ * Author: Artūrs Znotiņš
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package lv.lumii.ner;
 
 import java.io.FileNotFoundException;
@@ -17,11 +34,6 @@ import edu.stanford.nlp.objectbank.ObjectBank;
 import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
 import edu.stanford.nlp.sequences.LVCoNLLDocumentReaderAndWriter;
 import edu.stanford.nlp.util.StringUtils;
-
-/**
- * @author Artūrs
- *
- */
 public class NerPipe {
 	private static enum inputTypes {SENTENCE, CONLL};
 	private static enum outputTypes {CONLL_X, SIMPLE, INFEATURES};
