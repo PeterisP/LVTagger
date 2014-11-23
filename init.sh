e20b9b7cb3a9d63eb730fe8f3fe0dc7f9295a6d6
Didzis Gosko <didzis.gosko@gmail.com>
2014-11-23 22:12:32 +0200

    Changed paths.

--- init.sh
+++ init.sh
@@ -3,15 +3,11 @@
 REPDIR=`git rev-parse --show-toplevel`
 HOOKDIR=$REPDIR/.git/hooks
 
-# if [ -d $HOOKDIR ]; then
-# 	mv $HOOKDIR $REPDIR/.git/hooks_old
-# fi
-
 # set-up post-merge hook
 if [ ! -L $HOOKDIR/post-merge ]; then
-	ln -s -f ../../git_hooks/post-merge $HOOKDIR/post-merge
+	ln -s -f ../../.hooks/post-merge $HOOKDIR/post-merge
 fi
 
 # execute post-merge
-git_hooks/post-merge
+.hooks/post-merge
 
