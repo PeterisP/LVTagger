b6234636c40f274536cc5272c5fed9d4dd3a552b
arturs <arturs.znotins@gmail.com>
2015-03-16 16:36:16 +0200

    Fix nerPipe: byteStream to stringWriter

--- src/lv/lumii/ner/NerPipe.java
+++ src/lv/lumii/ner/NerPipe.java
@@ -19,14 +19,11 @@ package lv.lumii.ner;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
 import edu.stanford.nlp.ie.ListNERSequenceClassifier;
@@ -148,7 +145,7 @@ public class NerPipe {
 	}
 	
 	public String getAnswerString(List<CoreLabel> doc, DocumentReaderAndWriter<CoreLabel> rw) {
-		ByteOutputStream bos = new ByteOutputStream();
+		StringWriter bos = new StringWriter();
 		PrintWriter printer = new PrintWriter(bos);
 		rw.printAnswers(doc, printer);
 		return bos.toString();
