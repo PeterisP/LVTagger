a46f02c1236e6e7779b9816a504a0e40e8a5f39e
viestursjl <viesturs.j.lasmanis@gmail.com>
2024-02-28 17:23:06 +0200

    Piekārtoju kodu sakarā ar Multithreading izmaiņām.

--- morphotagger.sh
+++ morphotagger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $(dirname $0)
-java -mx1200m -cp "target/*" lv.lumii.morphotagger.MorphoPipe $*
+java -mx1200m -cp "target/*" lv.lumii.morphotagger.MultithreadingMorphoPipe $*
 # /Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home/bin/java -Xmx2G -cp dist/CRF.jar:dist/morphology.jar:dist/transliterator.jar:lib/json-simple-1.1.1.jar lv.lumii.morphotagger.MorphoPipe $*
